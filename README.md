fe66b771d613ffe90b63dae03a2468d7e9052938
Adrian Sadłocha <adrian.adek@gmail.com>
2017-03-24 22:18:40 +0100

    Add Flask-SQLAlchemy

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Server for _nameFactory_ project made using Python 3 and Flask.
 
 Install dependencies by running `pip install -r requirements.txt`.
 
+To initialize database, run `python init_db.py`. **Warning**: it will remove existing database.
+
 ## Running
 
 For development purposes running `./server.py` should be sufficient.
--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 @app.route('/')
