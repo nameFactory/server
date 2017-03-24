cec0f12efe222ac50e419a6f197f86a3f559a095
Adrian Sadłocha <adrian.adek@gmail.com>
2017-03-24 22:38:29 +0100

    Add routes for User

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,10 +18,26 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 
 @app.route('/')
 def hello():
     return 'Hello.'
+
+
+@app.route('/user')
+def get_users():
+    return jsonify([u.as_dict() for u in User.query.all()])
+
+
+@app.route('/user/<int:user_id>')
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        return jsonify(user.as_dict())
+    return jsonify({'errors': ['User not found']})
 
 
 if __name__ == '__main__':
